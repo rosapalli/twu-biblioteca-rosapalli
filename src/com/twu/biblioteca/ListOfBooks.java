d27d93c8311d6eda7aa031160b9f0ed0c1bf7376
Rosa Palli <rosapalli@Rosas-MacBook-Pro.local>
2019-08-12 17:30:39 +0200

    First structure outline

---
+++ src/com/twu/biblioteca/ListOfBooks.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+import java.util.*;
+
+//should have column separators
+
+public class ListOfBooks {
+
+    private ArrayList<Book> listOfBooks;
+
+    public void createListOfBooks() {
+
+    }
+
+}
