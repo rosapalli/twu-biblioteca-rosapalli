6048476e12496420a6476eef88cdbc791fd28e42
Rosa Palli <rosapalli@Rosas-MacBook-Pro.local>
2019-08-14 13:27:49 +0200

    release 1 completed

--- src/com/twu/biblioteca/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-package com.twu.biblioteca;
-
-public class WelcomeMessage {
-
-    public void displayWelcomeMessage() {
-        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-    }
-
-}
