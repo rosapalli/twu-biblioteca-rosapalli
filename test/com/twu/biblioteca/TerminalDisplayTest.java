6a7bbf7eda8fc00e01b97b5441ac880086a14961
Rosa Palli <rosapalli@Rosas-MacBook-Pro.local>
2019-08-19 10:43:08 +0200

    Biblioteca class refactor

---
+++ test/com/twu/biblioteca/TerminalDisplayTest.java
@@ -0,0 +1,29 @@
+package com.twu.biblioteca;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class TerminalDisplayTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(TerminalDisplay.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void captureUserInput() {
+    }
+
+    @Test
+    public void displayUserCurrentItemsCheckedOut() {
+    }
+}
